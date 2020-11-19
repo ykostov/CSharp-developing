e3bf73605d47b3a0632b4485ad9eab2bdf9bba86
Yasen Kostov <qskatak04@gmail.com>
2020-11-19 08:47:59 +0200

    First Commit
    Complete program

---
+++ Algorithms Ex.1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algorithms_Ex._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, S = 0;
+
+            a = int.Parse(textBox1.Text);
+            
+            if (a < 200 && a > 0)
+            {
+                label4.Visible = false;
+                b = a;
+                while (b <= 200)
+                {
+                    S = S + b;
+                    b = b + 1;
+                }
+                label2.Text = "" + S;
+            }
+            else
+            {
+                label4.Visible = true;
+            }
+
+        }
+    }
+}
